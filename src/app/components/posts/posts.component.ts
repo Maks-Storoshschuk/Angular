f481f83ced9c8b60df626af86c5c6f20cb69994d
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-11-15 15:01:15 +0200

    1 lesson Users + Posts with userId???

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Post} from "../../models/Post";
 import {PostService} from "../../services/post.service";
-import {UserComponent} from "../user/user.component";
-import {UsersComponent} from "../users/users.component";
 
 @Component({
   selector: 'app-posts',
@@ -12,14 +10,13 @@ import {UsersComponent} from "../users/users.component";
 export class PostsComponent implements OnInit {
 
   posts: Post[] = []
-  private userId: number = 2;
 
-  constructor(private postService:PostService) {
+  constructor(private postService: PostService) {
 
   }
 
   ngOnInit(): void {
-    this.postService.getPost(this.userId).subscribe(value=>this.posts = value)
+    this.postService.getPosts().subscribe(value => this.posts = value)
   }
 
 }
