3af9c8af361b43e3fed33b472d690996dd94179b
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-11-15 14:30:10 +0200

    1 lesson Users + Posts

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,4 +1,29 @@
-export interface User{
-  name:string;
-  email:string;
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: IAddress;
+  phone: string;
+  website: string;
+  company: ICompany;
+}
+
+interface IAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: IGeo;
+}
+
+interface IGeo {
+  lat: number;
+  lng: number;
+}
+
+interface ICompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
