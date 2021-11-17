60ea25bb7f70e340adf7876b7f37a672b2662327
Maks-Storoshschuk <maksstoroschuk@gmail.com>
2021-11-17 13:15:15 +0200

    lesson2-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
+import {UserComponent} from './components/user/user.component';
+import {UsersComponent} from './components/users/users.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {PostComponent} from './components/post/post.component';
 import {RouterModule} from "@angular/router";
-import { CommentComponent } from './components/comment/comment.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {UserDetailComponent} from './components/user-detail/user-detail.component';
+import {UserService} from "./services/user.service";
+import {UserResolveService} from "./services/user-resolve.service";
 
 @NgModule({
   declarations: [
@@ -28,18 +30,23 @@ import { UserDetailComponent } from './components/user-detail/user-detail.compon
     HttpClientModule,
     RouterModule.forRoot([
       {
-        path:'users',
-        component:UsersComponent,
-        children:[
-          {path:':id', component:UserDetailComponent},
+        path: 'users',
+        component: UsersComponent,
+        children: [
+          {
+            path: ':id',
+            component: UserDetailComponent,
+            resolve: {data: UserResolveService}
+          },
         ]
       },
 
-      {path:'posts', component:PostsComponent},
-      {path:'comments', component:CommentsComponent}
+      {path: 'posts', component: PostsComponent},
+      {path: 'comments', component: CommentsComponent}
     ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/user-detail/user-detail.component.ts
+++ src/app/components/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UserService} from "../../services/user.service";
+import {User} from "../../models/User";
 
 @Component({
   selector: 'app-user-detail',
@@ -6,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private userService: UserService) {
+    // this.activatedRoute.params.subscribe(params => {
+    //   let id = params['id'];
+    //   userService.getUser(+id).subscribe(value => {
+    //     this.user = value
+    //   })
+    // })
+    this.activatedRoute.data.subscribe(value => this.user = value['data']);
+  }
 
   ngOnInit(): void {
   }
